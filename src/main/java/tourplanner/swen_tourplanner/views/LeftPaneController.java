26483a8ef110872cf500f658935adb3b0071bef6
juliob <juliobampi97@gmail.com>
2023-05-13 16:11:45 +0200

    Added fxml components to the controller

--- src/main/java/tourplanner/swen_tourplanner/views/LeftPaneController.java
+++ src/main/java/tourplanner/swen_tourplanner/views/LeftPaneController.java
@@ -1,8 +1,35 @@
 package tourplanner.swen_tourplanner.views;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextField;
 import tourplanner.swen_tourplanner.viewModels.LeftPaneViewModel;
 
 public final class LeftPaneController implements ITourPlannerController {
+
+    //region Fxml Elements
+    @FXML
+    public Button createNewTourBtn;
+
+    //region ContextMenuItems
+    @FXML
+    public MenuItem cMenuNew;
+
+    @FXML
+    public MenuItem cMenuDelete;
+
+    @FXML
+    public MenuItem cMenuOpt;
+    //endregion
+
+    @FXML
+    public ListView toursListView;
+
+    @FXML
+    public TextField toursSearchTextInput;
+    //endregion
 
     private final LeftPaneViewModel leftPaneViewModel;
     public LeftPaneController(LeftPaneViewModel leftPaneViewModel) {
@@ -10,6 +37,7 @@ public final class LeftPaneController implements ITourPlannerController {
     }
 
     @Override
+    @FXML
     public void initialize() {
 
     }
